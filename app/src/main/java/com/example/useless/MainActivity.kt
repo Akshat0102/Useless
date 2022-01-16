5a4985dd4ed2cce31d83616012cadf73cab68a17
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-01-16 17:07:48 +0530

    added logic

--- app/src/main/java/com/example/useless/MainActivity.kt
+++ app/src/main/java/com/example/useless/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.useless
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.click)
+
+        button.setOnClickListener {
+            readIt()
+        }
+    }
+
+    private fun readIt() {
+
+        val message =
+            "The button says not to touch it.\n\nGet yourself a pair of glasses! \nYou Nitwit!"
+
+        AlertDialog.Builder(this)
+            .setTitle("Warning!")
+            .setCancelable(true)
+            .setMessage(message)
+            .show()
     }
 }
